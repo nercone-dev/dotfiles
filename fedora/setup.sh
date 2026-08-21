f8817d860a290776da2ac02546d7c02178120d86
nercone <nercone@nercone.dev>
2026-08-22 03:27:18 +0900

    Update setup.sh

--- fedora/setup.sh
+++ fedora/setup.sh
@@ -31,22 +31,22 @@ sudo /usr/bin/dnf install -y w3m elinks
 NONINTERACTIVE=1 /bin/bash -c "$(/usr/bin/curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # -- Utility
-/opt/homebrew/bin/brew install curl nano tree htop btop fastfetch ipatool qemu wakeonlan ffmpeg localsend tailscale
+/home/linuxbrew/.linuxbrew/bin/brew install curl nano tree htop btop fastfetch ipatool qemu wakeonlan ffmpeg localsend tailscale
 
 # -- Develop
-/opt/homebrew/bin/brew install git gh make cmake llvm ninja radare2
+/home/linuxbrew/.linuxbrew/bin/brew install git gh make cmake llvm ninja radare2
 
 # -- Editor
-/opt/homebrew/bin/brew install vim neovim nano
+/home/linuxbrew/.linuxbrew/bin/brew install vim neovim nano
 
 # -- Security
-/opt/homebrew/bin/brew install nmap osv-scanner openssl@3 openssl@4 gnupg pinentry
+/home/linuxbrew/.linuxbrew/bin/brew install nmap osv-scanner openssl@3 openssl@4 gnupg pinentry
 
 # -- Compression
-/opt/homebrew/bin/brew install zip xz gzip sevenzip woff2
+/home/linuxbrew/.linuxbrew/bin/brew install zip xz gzip sevenzip woff2
 
 # -- Web Browser
-/opt/homebrew/bin/brew install w3m felinks chawan
+/home/linuxbrew/.linuxbrew/bin/brew install w3m felinks chawan
 
 # oh-my-zsh
 /bin/sh -c "$(/usr/bin/curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
